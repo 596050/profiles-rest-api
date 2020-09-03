1bb8b9efd3e46f05b70bf29867822f2bf0660cbe
Tom <pricet@Toms-MacBook-Pro.local>
2020-09-03 18:47:22 +0100

    Add user-profile-feast-it.herokuapp.com to allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'user-profile-feast-it.herokuapp.com'
+]
 
 
 # Application definition
