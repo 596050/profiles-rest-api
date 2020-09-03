55b339ab21d789ded3c604edb24900333b934e31
Tom <pricet@Toms-MacBook-Pro.local>
2020-09-03 20:35:51 +0100

    Add cors

--- profiles_project/middleware.py
+++ profiles_project/middleware.py
@@ -5,6 +5,7 @@ class MyMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        response['Access-Control-Allow-Origin'] = "*"
+        response['Access-Control-Allow-Origin'] = "http://localhost:3000"
+        response['Vary'] = "Origin"
         response['Access-Control-Allow-Credentials'] = True
         return response
